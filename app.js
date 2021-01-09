9df898d97c52d10e521fa804899afe1270a69002
Shubham656jain <shubham656jain@gmail.com>
2021-01-09 11:22:21 +0530

    new changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
+
 
 const Product = require('./models/product');
 
@@ -17,18 +19,19 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
 
 app.get('/products', async (req,res) => {
     const products = await Product.find({});
-    console.log(products);
     res.render('products/index', {products:products})
 })
 
 app.post('/products', async (req,res) =>{
     const newProd = new Product(req.body);
     await newProd.save();
-    console.log(newProd);
     res.redirect('/products');
 })
 
@@ -39,11 +42,20 @@ app.get('/products/new', (req,res) =>{
 app.get('/products/:id', async (req,res) => {
     const { id } = req.params;
     const p = await Product.findById(id);
-    console.log(p);
     res.render('products/show', {p});
 
 })
 
+app.get('/products/:id/edit', async (req,res) =>{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', {product:product});
+})
+
+app.put('/products/edit/:id', async (req,res) =>{ 
+    console.log(req.body);
+    res.send("A put request!!");
+})
 
 app.listen(3000, ()=>{
     console.log("Server Started on port 3000");
